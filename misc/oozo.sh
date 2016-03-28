8ca700f5de8b17d9ca22f696c43fa7abb17df1a7
juliano piassa <juliano@oozo.tv>
2016-03-28 10:25:50 -0300

    oozo.sh atualizado

--- misc/oozo.sh
+++ misc/oozo.sh
@@ -1,115 +1,137 @@
 #!/bin/bash
-# Instala os pacotes necessarios para uso da OOZO.TV em ambiente LINUX usando Raspberry Pi
-# Os arquivos da OOZO devem ser copiados para o Desktop
-# Feito por Juliano Piassa | juliano@oozo.tv
+# iNSTALL ALL NECESSARY FILES TO USE OOZO.TV ON A RASPBERRY PI.
+# Made by Juliano Piassa | juliano@oozo.tv
 # http://www.oozo.tv
 
-echo "Installing OOZO.tv"
-sleep 5	
-echo "Updating and upgrading the system, (grab a book and chill...)"
-# 1. Create necessary directories 
+echo " Main script to use OOZO.tv on a raspberry pi." 
+
+echo "YOU must be root."
+echo "YOU must be root."
+echo "YOU must be root."
+echo "YOU must be root."
+echo "YOU must be root."
+echo "YOU must be root."
+echo "YOU must be root."
+echo "YOU must be root."
+echo "YOU must be root."
+echo "YOU must be root."
+echo "YOU must be root."
+echo "YOU must be root."
+echo "YOU must be root."
+echo "YOU must be root."
+echo "YOU must be root."
+echo "YOU must be root."
+echo "YOU must be root."
+echo "YOU must be root."
+echo "YOU must be root."
+echo "YOU must be root."
+
+sudo mkdir /home/pi/oozotv
 sudo mkdir /home/pi/.config/autostart
-#Update and Upgrade the system, should take a while
-sudo apt-get -qq update
-sudo apt-get -y -qq upgrade
-sudo apt get -y install ntpdate ntp xscreensaver python-pip libgtk-3-dev fbi watchdog git-core chkconfig
-sudo pip install wifi
-sudo pip install configparser
-echo "cloning Git.. Please, enter user and passwd to repo"
-git clone -q https://github.com/OOZOtv/OOZObox.git "$HOME/oozo" >
+sudo mkdir /home/pi/oozotv/logs
+sudo mkdir /home/pi/oozotv/oozo_files
+sudo mkdir /home/pi/oozotv/screens
+sudo mkdir /home/pi/oozotv/speedtest
+sudo mkdir /home/pi/oozotv/ping
 
-#2. Download necessary files to OOZO.tv
-echo "Downloading necessary files ..."
+#Download all necessary files.
+sudo wget --no-check-certificate -P /usr/local/bin https://www.dropbox.com/s/20krtcpx7zon7wc/checkwifi_restart.sh?dl=1
+sudo wget --no-check-certificate -P /usr/local/bin https://www.dropbox.com/s/ovsdn45u7xmg1lr/checkwifi.sh?dl=1
+sudo wget --no-check-certificate -P /usr/local/bin https://www.dropbox.com/s/92kr6lgrqfzguql/checketh.sh?dl=1
+sudo wget --no-check-certificate -P /home/pi https://www.dropbox.com/s/u9jcqe1ztr9il7l/oozo.tar?dl=1
+sudo wget --no-check-certificate -P /home/pi/oozotv https://www.dropbox.com/s/kd2u6qs5rdo8dor/desktop-background.png?dl=1
+#sudo wget --no-check-certificate -P /home/pi/oozotv http://steinerdatenbank.de/software/kweb3-1.6.4-autoplay.tar.gz
+sudo wget --no-check-certificate -P /home/pi/oozotv https://www.dropbox.com/s/66syjt6vmgzkttf/oozo.desktop?dl=1
+sudo wget --no-check-certificate -P /home/pi/.config/autostart https://www.dropbox.com/s/66syjt6vmgzkttf/oozo.desktop?dl=1
+sudo wget --no-check-certificate -P /home/pi/.config/pcmanfm/LXDE-pi https://www.dropbox.com/s/n0r8wqdjj494apo/desktop-items-0.conf?dl=1
+sudo wget --no-check-certificate -P /home/pi/oozotv https://www.dropbox.com/s/e84x86odlmf56ku/NTP_update.sh?dl=1
+sudo wget --no-check-certificate -P /usr/local/bin https://www.dropbox.com/s/ovsdn45u7xmg1lr/checkwifi.sh?dl=1
+sudo wget --no-check-certificate -P /home/pi/oozotv https://www.dropbox.com/s/g7z8sy24jrfg75g/config.txt?dl=1
+sudo wget --no-check-certificate -P /home/pi/oozotv https://www.dropbox.com/s/j2031636mppik6b/crontab?dl=1
+sudo wget --no-check-certificate -P /etc https://www.dropbox.com/s/owmcd3mcfffp8lf/splash.png?dl=1
+sudo wget --no-check-certificate -P /home/pi/oozotv https://www.dropbox.com/s/wbirxsn08czbml4/lightdm.conf?dl=1
+sudo wget --no-check-certificate -P /home/pi/oozotv/speedtest https://raw.github.com/sivel/speedtest-cli/master/speedtest_cli.py
+sudo wget --no-check-certificate -P /home/pi/oozotv https://www.dropbox.com/s/e4vj0erki1a8ph7/autostart?dl=1 
+sudo wget --no-check-certificate -P /home/pi/oozotv https://www.dropbox.com/s/103pnsesluicid9/hostname?dl=1
+sudo wget --no-check-certificate -P /home/pi/oozotv https://www.dropbox.com/s/bv4wn0b1b2z6q7q/hosts?dl=1
+#sudo wget --no-check-certificate -P /etc/init.d https://www.dropbox.com/s/qq7ndjzwl6bp0q8/asplashscreen.sh?dl=1
 
-sudo mv /home/pi/oozo/misc/checkwifi_restart.sh /usr/local/bin
-sudo mv /home/pi/oozo/misc/checkwifi.sh /usr/local/bin
-sudo mv /home/pi/oozo/misc/checketh.sh /usr/local/bin
-sudo wget --no-check-certificate -P /home/pi/oozo https://s3-sa-east-1.amazonaws.com/oozofiles/oozo.tar
-sudo wget --no-check-certificate -P /home/pi/oozo https://s3-sa-east-1.amazonaws.com/oozofiles/desktop-background.png
-sudo wget --no-check-certificate -P /home/pi/oozo https://s3-sa-east-1.amazonaws.com/oozofiles/kweb3-1.6.4-autoplay.tar.gz
-sudo wget --no-check-certificate -P /home/pi/oozo https://s3-sa-east-1.amazonaws.com/oozofiles/oozo.desktop
-sudo wget --no-check-certificate -P /home/pi/.config/autostart https://s3-sa-east-1.amazonaws.com/oozofiles/oozo.desktop
-sudo wget --no-check-certificate -P /home/pi/.config/pcmanfm/LXDE-pi https://s3-sa-east-1.amazonaws.com/oozofiles/desktop-items-0.conf
-sudo wget --no-check-certificate -P /etc https://s3-sa-east-1.amazonaws.com/oozofiles/splash.png
-sudo mv /home/pi/oozo/misc/NTP_update.sh /etc/init.d/NTP_update.sh
-sudo mv /home/pi/oozo/misc/config.txt /boot/config.txt
-sudo mv /home/pi/oozo/misc/asplashscreen.sh /etc/init.d 
-sudo mv /home/pi/oozo/misc/reboot.sh /usr/local/bin
-sudo mv /home/pi/oozo/misc/update_slave.sh /usr/local/bin
-sudo rm /boot/cmdline.txt
-sudo mv /home/pi/oozo/misc/cmdline.txt /boot/cmdline.txt
-sudo cp /etc/init.d/NTP_update.sh /usr/local/bin/NTP_update.sh
-sudo cp /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
-sudo crontab /home/pi/oozo/misc/crontab
-sudo rm /home/pi/.xscreensaver
-sudo mv /home/pi/oozo/misc/OI.xscreensaver /home/pi/.xscreensaver
-# Install supervisor using pip
-sudo pip install supervisor==3.2.0 -q
-sudo mkdir /var/log/supervisor
-sudo mkdir -p /etc/supervisor/conf.d
-#Untar oozo files on /home/pi
-sudo tar -xzvf /home/pi/oozo/oozo.tar
-#Untar autoplay update for Kweb3 GTK3 on /home/pi
-tar -xzvf /home/pi/oozo/kweb3-1.6.4-autoplay.tar.gz
-#Permissions 
-sudo chmod a+x /etc/init.d/asplashscreen.sh
+#Rename files 
+sudo rm /etc/hosts
+sudo rm /etc/hostname
+sudo rm /boot/config.txt
+sudo rm /etc/lightdm/lightdm.conf
+sudo rm /home/pi/.config/lxsession/LXDE-pi/autostart
+#sudo mv /etc/init.d/asplashscreen.sh?dl=1 /etc/init.d/asplashscreen.sh
+#sudo mv /etc/splash.png?dl=1 /etc/splash.png
+sudo mv /home/pi/oozotv/hostname?dl=1 /etc/hostname
+sudo mv /home/pi/oozotv/hosts?dl=1 /etc/hosts
+sudo mv /home/pi/oozotv/autostart?dl=1 /home/pi/.config/lxsession/LXDE-pi/autostart
+sudo mv /home/pi/oozotv/lightdm.conf?dl=1 /etc/lightdm/lightdm.conf
+sudo mv /home/pi/oozotv/config.txt\?dl\=1 /boot/config.txt
+sudo mv /home/pi/oozotv/oozo.desktop?dl=1 /home/pi/Desktop/oozo.desktop
+sudo mv /home/pi/oozotv/desktop-background.png?dl=1 /home/pi/oozotv/oozo_files/desktop-background.png
+sudo mv /home/pi/oozo.tar?dl=1 /home/pi/oozo.tar
+#sudo mv /home/pi/.config/autostart/oozo.desktop?dl=1 /home/pi/.config/autostart/oozo.desktop
+sudo mv /home/pi/oozotv/NTP_update.sh?dl=1 /home/pi/oozotv/NTP_update.sh
+sudo mv /home/pi/oozotv/NTP_update.sh /etc/init.d/NTP_update.sh
+sudo mv /usr/local/bin/checkwifi.sh?dl=1 /usr/local/bin/checkwifi.sh
+sudo mv /usr/local/bin/checkwifi_restart.sh?dl=1 /usr/local/bin/checkwifi_restart.sh
+sudo mv /usr/local/bin/checketh.sh?dl=1 /usr/local/bin/checketh.sh
+sudo mv /home/pi/.config/pcmanfm/LXDE-pi/desktop-items-0.conf?dl=1 /home/pi/.config/pcmanfm/LXDE-pi/desktop-items-0.conf
+sudo mv /home/pi/oozotv/crontab?dl=1 /home/pi/oozotv/crontab
+
+sudo tar -xvf oozo.tar
+sudo mv /home/pi/oozo_init /home/pi/oozotv
+sudo tar -xzf kweb3-1.6.9-autoplay.tar.gz
+sudo apt-get -y install ntpdate 
+sudo apt-get -y install ntp
+sed -i 's/\r$//' /etc/init.d/NTP_update.sh
+#sudo apt-get -y install xscreensaver
+sudo crontab /home/pi/oozotv/crontab
+#Give all permissions
+#sudo chmod a+x /etc/init.d/asplashscreen.sh
+sudo chmod 644 /etc/hostname
+sudo chmod 644 /etc/hosts
+sudo chmod +x /home/pi/oozotv/speedtest/speedtest_cli.py
 sudo chmod +x /usr/local/bin/checkwifi.sh
 sudo chmod +x /usr/local/bin/checketh.sh
 sudo chmod +x /usr/local/bin/checkwifi_restart.sh
-sudo chmod +x /home/pi/oozo/oozo_init/debinstall
-sudo chmod +x /home/pi/oozo/oozo_init/install
-sudo chmod +x /home/pi/oozo/oozo_init/ktop
-sudo chmod +x /home/pi/oozo/oozo_init/prepare
-sudo chmod +x /home/pi/oozo/oozo_init/remove
-sudo chmod +x /home/pi/oozo/oozo_init/removeall
-sudo chmod +x /home/pi/oozo/oozo_init/oozo_init.py
+sudo chmod +x debinstall install ktop prepare remove removeall
+sudo chmod +x /home/pi/oozotv/oozo_init/oozo_init.py
 sudo chmod +x /etc/init.d/NTP_update.sh
-sudo chmod 775 /home/pi/oozo/misc/crontab
+sudo chmod -R 775 /home/pi/oozotv/oozo_init
+sudo chmod 775 /home/pi/oozotv/crontab
 sudo chmod 775 /usr/local/bin/checkwifi.sh
 sudo chmod 775 /usr/local/bin/checketh.sh
 sudo chmod 775 /usr/local/bin/checkwifi_restart.sh
 sudo chmod 775 /etc/init.d/NTP_update.sh
-sudo chmod +x /usr/local/bin/reboot.sh
-sudo chmod 775 /usr/local/bin/reboot.sh
-sudo chmod +x /usr/local/bin/update_slave.sh
-sudo chmod 775 /usr/local/bin/update_slave.sh
-
-echo "Not-Ready-yet! :P"
-
-#Config watchdog
-sudo modprobe bcm2708_wdog
-sudo cp /etc/modules /etc/modules.bak
-sudo sed '$ i\bcm2708_wdog' -i /etc/modules
-sudo chkconfig watchdog on
-sudo cp /etc/watchdog.conf /etc/watchdog.conf.bak
-sudo sed -e 's/#watchdog-device/watchdog-device/g' -i /etc/watchdog.conf
-sudo /etc/init.d/watchdog start
-
-#Supervisor...
-
-#Services and updates
+sudo chmod 777 /home/pi/.config/lxsession/LXDE-pi/autostart
+#sudo chmod 777 /home/pi/.config/autostart/oozo.desktop
 update-rc.d NTP_update.sh defaults
 sudo wget https://yt-dl.org/latest/youtube-dl -O /usr/bin/youtube-dl
 sudo chmod a+x /usr/bin/youtube-dl
-sudo chmod 777 /etc/network/interfaces
-cd /home/pi/oozo/oozo_init
+#sudo apt-get -y update 
+sudo apt-get -y install fbi
+#sudo apt-get -y install libgtk-3-dev
+sudo apt-get -y install python-pip
+sudo pip install wifi
+sudo pip install configparser
+sudo chmod 755 /etc/network/interfaces
+#sudo dpkg -i libavformat53_0.8.17-1+rp1_armhf.deb
 sudo ./debinstall
-sudo insserv /etc/init.d/asplashscreen
-sudo mv kweb3 /usr/bin
-sudo insserv /etc/init.d/asplashscreen.sh
-sudo update-rc.d /etc/init.d/asplashscreen.sh start
+#sudo insserv /etc/init.d/asplashscreen
+#sudo mv kweb3 /usr/bin
+sudo chmod -R 777 /home/pi/oozotv/oozo_init
 sh -c 'setterm -blank 0 -powersave off -powerdown 0 < /dev/console > /dev/console 2>&1'
-
-#Unclutter - Ride mouse
 sudo apt-get -y install unclutter
 unclutter -idle 1 -root &
-
-sudo mv /home/pi/check.py /home/pi/debinstall /home/pi/install /home/pi/ktop /home/pi/kweb3-1.6.4-autoplay.tar.gz /home/pi/kweb_1.6.4-1_armhf.deb /home/pi/libavformat53_0.8.17-1+rpi1_armhf.deb /home/pi/oozo.sh /home/pi/oozo.tar /home/pi/prepare /home/pi/desktop-background.png /home/pi/remove /home/pi/removeall /home/pi/oozo_files
+sudo mv /home/pi/check.py /home/pi/debinstall /home/pi/install /home/pi/ktop /home/pi/kweb3-1.6.4-autoplay.tar.gz /home/pi/kweb_1.6.4-1_armhf.deb /home/pi/libavformat53_0.8.17-1+rpi1_armhf.deb /home/pi/oozo.sh /home/pi/oozo.tar /home/pi/prepare /home/pi/remove /home/pi/removeall /home/pi/oozotv/oozo_files
 
 echo "
-###################################################
-#Instalation finished, rebooting the Raspberry Pi.#
-###################################################"
+##########################################################################################################
+#OOZO.tv ready. Restarting the system NOW. If you want to cancel, please, CTRL + C will do the trick. Cya#
+##########################################################################################################"
 sleep 1
 echo "10 ..."
 sleep 1
@@ -133,7 +155,7 @@ echo "1 ..."
 sleep 1
 
 sleep 2
-sudo reboot
+#sudo reboot
 
 exit
-# FIM
+#This_is_the_End
