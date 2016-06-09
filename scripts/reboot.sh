02533dde9996413a98d76dd92615cbf8c7c2b816
juliano piassa <juliano@oozo.tv>
2016-06-09 10:09:27 -0300

    remove old scripts

--- scripts/reboot.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#reinicia equipamento
-sudo reboot
--- scripts/update.sh
+++
@@ -1,9 +0,0 @@
-#OOZO UPDATER
-
-#sudo wget --no-check-certificate -P /home/pi/Desktop https://www.dropbox.com/s/ok4yi3yibfjr5m7/2387974302_c943e77388_o.jpg?dl=1
-#sleep 3
-
-
-
-
-
